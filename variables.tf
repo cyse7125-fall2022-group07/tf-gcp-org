07d9ed0dd6273ad54d2021f2aaacf2980c48cbe6
Shrawani <karpe.s@northeastern.edu>
2022-09-30 17:50:08 -0400

    gcp org id

--- variables.tf
+++ variables.tf
@@ -1,4 +1,6 @@
 variable "gcp_org"{
     description = "gcp organization"
-    default = "1030184620162"
+    default = "1030184620162" #shrawani
+    # default = "1070836565374" #vinit
+    # default = "801298137383" #harshal
 }
