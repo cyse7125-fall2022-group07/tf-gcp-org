ea6c19a772a42e0e809a715417446e620b122a11
Shrawani <karpe.s@northeastern.edu>
2022-09-30 18:03:11 -0400

    vars changed

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "google_folder" "adv-cloud" {
     display_name = "adv-cloud"
-    parent       = "organizations/1030184620162"
+    parent       = "organizations/${var.gcp_org}"
 }
 
 resource "google_project" "assignment-2" {
     name       = "assignment-2"
-    project_id = "assignment-2-28-92022"
+    project_id = var.project_id
     folder_id  = google_folder.adv-cloud.name
 }   
 
@@ -13,6 +13,6 @@ resource "google_organization_policy" "skip_constraint" {
     org_id = var.gcp_org
     constraint = "compute.skipDefaultNetworkCreation"
     boolean_policy {
-    enforced = true
+        enforced = true
     }
 }
--- variables.tf
+++ variables.tf
@@ -4,3 +4,9 @@ variable "gcp_org"{
     # default = "1070836565374" #vinit
     # default = "801298137383" #harshal
 }
+
+variable "project_id"{
+    default = "assignment-2-28-92022" #shrawani
+    # default = "vharsora-1701" #vinit
+    # default = "assignment-30-9-2022-602" #harshal
+}
